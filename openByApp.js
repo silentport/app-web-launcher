675926d92cc40ace72bc11607bdf86e13547ee19
silentport <liyonglong@xiaomi.com>
2018-10-11 17:28:22 +0800

    Update openByApp.js

--- openByApp.js
+++ openByApp.js
@@ -4,7 +4,7 @@ function openByApp(option) {
   var iframe = null;
   var Threshold = option.hangUp < 600 ? 600 : option.hangUp ; //h5页面挂起到恢复的最大时间
   
-  option.first && option.first.bind(option.context)();
+  option.enter && option.enter.bind(option.context)();
   document.removeEventListener("visibilitychange", window.handleHidePage);
   tryTime = new Date();
   
@@ -21,7 +21,7 @@ function openByApp(option) {
     // 挂起时长小于Threshold, 可以认为H5没有挂起,允许下载
     if (endTime - tryTime < Threshold) {
         location.href = option.downloadUrl;
-        option.callback && option.callback.bind(option.context)();
+        option.willDownLoad && option.willDownLoad.bind(option.context)();
     }
     if (iframe) {
         document.body.removeChild(iframe);
